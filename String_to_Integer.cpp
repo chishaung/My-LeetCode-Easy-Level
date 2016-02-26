7deaee82a918cfab28372e68535b2a19e1824b93
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2016-02-26 00:29:51 -0800

    Update String_to_Integer.cpp

--- String_to_Integer.cpp
+++ String_to_Integer.cpp
@@ -30,3 +30,48 @@ public:
     return num * sign;
     }
 };
+
+
+/*
+我自己寫的 無法通過 "   - 321"
+*/
+
+
+
+
+
+class Solution {
+public:
+    int myAtoi(string str) {
+        long result = 0L;
+        int count = 0;
+        int i = 0;
+        int flag = 0;
+        int length = str.length();
+        
+        while ( (!(str[i] >= '0') || !(str[i] < '9')) && i < length) 
+           i++;
+        
+
+        
+        if (str[i-1] == '+' && str[i-2] == '-') return 0;
+        
+        if (str[i-1] == '-') {
+            if (str[i-2] == '+') return 0;
+            flag = 1;
+        }
+        
+        //cout << "Flag: " << flag << endl;
+        
+        for (; i < length; i++) {
+            if (str[i] > '9' || str[i] < '0') break;
+            result = result * 10 + str[i] - '0';
+        }
+        
+        if (result > INT_MAX || result < INT_MIN) 
+            return result = flag ? INT_MIN : INT_MAX;
+        
+        else 
+            return result = (flag == 1) ? result * -1 : result;
+    }
+};
