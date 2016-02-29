e578b10829fb049662df249901e1aa460704040c
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2016-02-28 20:15:27 -0800

    Update Valid_Palindrome.cpp

--- Valid_Palindrome.cpp
+++ Valid_Palindrome.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        // 找出頭跟尾
         int begin, end;
         begin = 0; end = s.length()-1;
        
-        
+        // 開始LooP: head++ end-- 直到 end > head 就是找出中心點了
         while (end > begin) {
             if (!(isalnum(s[begin]))) begin++;
             else if (!(isalnum(s[end]))) end--;
