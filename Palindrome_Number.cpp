fb62bdca3efc31d9bf23333ea347c0cf961c8789
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2016-03-01 04:55:44 -0800

    Update Palindrome_Number.cpp

--- Palindrome_Number.cpp
+++ Palindrome_Number.cpp
@@ -1,3 +1,22 @@
+// 概念為：如果今天數字為回文 那即使是Reverse過後數值也會跟原本的數字相同大小
+// 比方說：50005, 2332, 4455665544
+class Solution {
+public:
+    bool isPalindrome(long long int x) {
+        if (x < INT_MIN || x > INT_MAX) return false;
+        int sum = 0;
+        int target = x;
+        while (x > 0) {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        
+        return sum == target;
+        
+        
+    }
+};
+
 
 // 這樣子做可以更加直觀 但是題目規定了不能佔用另外的額外空間
 // 因此還是要用上面的方式下去寫
