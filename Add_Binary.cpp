6c3f80485ffa1ae9ab3c2cd9034269de1a542ed2
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2016-10-05 23:00:58 -0700

    Update Add_Binary.cpp

--- Add_Binary.cpp
+++ Add_Binary.cpp
@@ -7,16 +7,22 @@ string addBinary(string a, string b) {
     reverse(b.begin(), b.end());
     int val = 0, carry = 0;
     
+    // 決定哪個字串比較長
     int length = (a.size() > b.size()) ? a.size() : b.size();
     for (int i = 0; i < length; i++) {
+        // 把短字串用0補的一樣長
         int av = a.size() > i ? a[i] - '0' : 0;
         int bv = b.size() > i ? b[i] - '0' : 0;
-        //cout << av << ' ' << bv << ' ' << length << endl;
+        
+        // 當下數字為0或1 -- 2 % 2 =0
         val = (av + bv + carry) % 2;
+        // 一樣的方法決定是否進位
         carry = (av + bv + carry) / 2;
+        
+        // 插入當下數字
         result.insert(result.begin(), val + '0');
     }
-    
+    // 若carry為1 代表最後有進位 補一個1到最前面
     if (carry == 1) result.insert(result.begin(), '1');
     
     
