227fb81bbc99804616534d93e65b07504620d906
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2016-11-03 23:33:47 -0700

    Create 2016.11.03 Arranging Coins.cpp

---
+++ 2016.11.03 Arranging Coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        long low = 1, high = n;
+        while (low < high) {
+            long mid = low + (high - low + 1) / 2;
+            if ((mid + 1) * mid / 2.0 <= n) low = mid;
+            else high = mid - 1;
+        }
+        return high;
+            
+    }
+};
